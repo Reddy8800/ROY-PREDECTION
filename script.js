89f7ec420b51ca0372540c93e78eaa446a460b24
ankit nishad <ankitnishad3333@gmail.com>
2025-12-04 10:12:05 +0530

    ds

---
+++ script.js
@@ -0,0 +1,8 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "ROY PREDECTION"; 
+document.getElementById("subscriberCount").innerText = "32,900+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
